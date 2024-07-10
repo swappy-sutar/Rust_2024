fcfc3b46b0c6673a35568af470e70ec6f8f2a845
swapnil <sutarswapnil322@gmail.com>
2024-07-10 17:53:36 +0530

    start the rust learning hello world

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+E:\ALL LANGUAGE'S CODES\RUST\hello_world\target\debug\deps\hello_world.exe: src/main.rs
+
+E:\ALL LANGUAGE'S CODES\RUST\hello_world\target\debug\deps\hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+E:\ALL\ LANGUAGE'S\ CODES\RUST\hello_world\target\debug\hello_world.exe: E:\ALL\ LANGUAGE'S\ CODES\RUST\hello_world\src\main.rs
